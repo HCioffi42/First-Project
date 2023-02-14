1c70bf5b895f27708b16a62d19012be8f6624faa
Henrique Cioffi <henrique.cioffi@atos.net>
2023-02-14 13:37:00 -0300

    Skeleton comments add

--- Enemy/Skeleton.cs
+++ Enemy/Skeleton.cs
@@ -1,27 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Collections;  // Import the System.Collections namespace to use non-generic collections
+using System.Collections.Generic;  // Import the System.Collections.Generic namespace to use generic collections
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Skeleton : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private NavMeshAgent agent;  // Reference to the NavMeshAgent component
 
-    private Player player;
+    private Player player;  // Reference to the player object
 
-    // Start is called before the first frame update
     void Start()
     {
-        player = FindObjectOfType<Player>();
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
+        player = FindObjectOfType<Player>();  // Find the player object in the scene
+        agent.updateRotation = false;  // Disable automatic rotation updates by NavMeshAgent
+        agent.updateUpAxis = false;  // Disable automatic up-axis updates by NavMeshAgent
     }
 
-    // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.transform.position);
-
-        
+        agent.SetDestination(player.transform.position);  // Set the destination of the NavMeshAgent to the player's position
     }
 }
