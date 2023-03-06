27f53e120d5b7df09b90f411d77421f5152f68f4
Henrique Cioffi <henrique.cioffi@atos.net>
2023-03-06 16:55:35 -0300

    Error corrections about the Filling amount on the HUD

--- Player_Itens.cs
+++ Player_Itens.cs
@@ -6,17 +6,17 @@ public class Player_Itens : MonoBehaviour
 {
     // These variables store the amounts of different items collected by the player.
     [Header("Amounts")]
-    public int totalWood;
-    public int totalCarrots;
+    public float totalWood;
+    public float totalCarrots;
     public float currentWater;
-    public int totalFish;
+    public float totalFish;
     
     // These variables define the limits for each type of item that the player can carry.
     [Header("Limits")]
-    public int woodLimit;
-    public int carrotsLimit;
+    public float woodLimit;
+    public float carrotsLimit;
     public float waterLimit;
-    public int fishLimit;
+    public float fishLimit;
 
     // This method is used to add water to the player's inventory, up to the water limit.
     public void WaterLimit(float water)
@@ -29,7 +29,7 @@ public class Player_Itens : MonoBehaviour
     }
 
     // This method is used to add wood to the player's inventory, up to the wood limit.
-    public void WoodLimit(int wood)
+    public void WoodLimit(float wood)
     {
         // If the total wood amount is less than the wood limit, add the given amount of wood to the inventory.
         if(totalWood < woodLimit)
@@ -39,17 +39,17 @@ public class Player_Itens : MonoBehaviour
     }
 
     // This method is used to add fish to the player's inventory, up to the fish limit.
-    public void FishLimit(int fish)
+    public void FishLimit(float fish)
     {
         // If the total fish amount is less than the fish limit, add the given amount of fish to the inventory.
-        if(totalFish < woodLimit)
+        if(totalFish < fishLimit)
         {
             totalFish += fish;
         }
     }
 
     // This method is used to add carrots to the player's inventory, up to the carrots limit.
-    public void CarrotsLimit(int carrots)
+    public void CarrotsLimit(float carrots)
     {
         // If the total carrot amount is less than the carrot limit, add the given amount of carrots to the inventory.
         if(totalCarrots < carrotsLimit)
